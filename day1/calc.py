6db1970d70dad0f78b5a5b7bc5524cfd18ce1acb
pinedab <brandyy_9490@hotmail.com>
2018-07-21 04:47:47 -0400

    more arithmetic

--- day1/calc.py
+++ day1/calc.py
@@ -1,4 +1,18 @@
 # calc
 
-print(1+2);
-print(3);
+#print(1+2);
+#print(3);
+
+####SIMPLE ARITHMETIC
+print(1+2)
+print(2*3)
+print(5-8)
+print(9/2)
+
+print()
+print(5 * (12-8) + -15)
+print(98 + (59872 / (13*8)) * -51)
+
+# power and bitshift
+print(2**8)
+print(1<<9)
