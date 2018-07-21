383e99fd4d92453177345912491554238c58e919
pinedab <brandyy_9490@hotmail.com>
2018-07-21 05:21:45 -0400

    modulo practice

--- day1/calc.py
+++ day1/calc.py
@@ -14,5 +14,15 @@ print(5 * (12-8) + -15)
 print(98 + (59872 / (13*8)) * -51)
 
 # power and bitshift
+print()
 print(2**8)
 print(1<<9)
+
+#modulo: gives remainder
+print()
+print(10%2)
+print(10%3)
+print(11%2)
+print(11%3)
+
+
