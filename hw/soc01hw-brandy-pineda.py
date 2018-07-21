e01fd64ad23cc339542364357151cb4e3874208a
pinedab <brandyy_9490@hotmail.com>
2018-07-21 05:24:06 -0400

    day1 hw solutions

---
+++ hw/soc01hw-brandy-pineda.py
@@ -0,0 +1,13 @@
+##### Write a program that tells you the following:
+
+# 1. Hours in a year. How many hours are in a year?
+# 2. Minutes in a decade. How many minutes are in a decade?
+# 3. Your age in seconds. How many seconds old are you? 
+# 4. Andreea Visanoiu​: I'm 48618000 seconds old hahaha. Calculate @Andreea Visanoiu's age.
+
+##### Here are some tougher questions:
+
+# 5. How many days does it take for a 32-bit system to timeout, if it has a bug with integer overflow?
+# 6. How about a 64-bit system? Computers run on milliseconds!
+# 7. Calculate your age accurately based on your birthday 
+# (maybe use time of day e.g. 8:23am if you know it, use 12:00 noon midday) - you will need Python modules.
