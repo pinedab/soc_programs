a7ed84edd4333039b05607d398fdb2825416ebe2
pinedab <brandyy_9490@hotmail.com>
2018-07-25 05:00:08 -0400

    d2 saving/reusing vars

--- day2/vars.py
+++ day2/vars.py
@@ -7,3 +7,23 @@ print()
 my_string = '...you can say that again...'
 print(my_string)
 print(my_string)
+
+print('...you can say that again...')
+print('...you can say that again...')
+
+print()
+print()
+
+my_string = '...you can say that again...'
+print(my_string)
+print(my_string)
+
+var1 = 5
+var2 = var1
+print(var1)
+print(var2)
+
+print()
+var1 = 'five'
+print(var1)
+print(var2)
