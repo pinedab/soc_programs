50d1c255fac4c47f1b9b72ba81d1b531eee27843
pinedab <brandyy_9490@hotmail.com>
2018-08-02 04:51:43 -0500

    while loops

---
+++ day4/loops.py
@@ -0,0 +1,16 @@
+##### LOOPS #####
+
+## WHILE ##
+i = 1
+while i < 6:
+	print(i)
+	i = i + 1 #crucial to include
+print('____\n')
+j = 0
+while j < 11:
+	print(j) #print HERE to show last count
+	if j == 3:
+		break # stops at j = 3
+		# print(j) #print HERE NOT to show last count
+	j += 1
+print('____\n')
