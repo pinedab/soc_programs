91f032b83cdb2692199248eb51ad2046d6a4dca0
pinedab <brandyy_9490@hotmail.com>
2018-08-02 01:39:26 -0500

    day2 hw

--- hw/soc01hw-brandy-pineda.py
+++ hw/soc01hw-brandy-pineda.py
@@ -1,5 +1,6 @@
 ##### Write a program that tells you the following:
 
+#####DAY1
 # 1. Hours in a year. How many hours are in a year?
 print(24 * 365)
 # 2. Minutes in a decade. How many minutes are in a decade?
@@ -8,5 +9,18 @@ print((60 * 24) *365* 10)
 print(60*60*(24 * 365 * 21))
 # 4. Andreea Visanoiu​: I'm 48618000 seconds old hahaha. Calculate @Andreea Visanoiu's age.
 print(48618000 / (60*60*24*365) * 10)
-##### Here are some tougher questions:
 
+#####DAY2 NONE
+#####DAY3
+# 5. Full name greeting. Write a program that asks for a 
+# person’s first name, then middle, and then last. Finally,
+# it should greet the person using their full name.
+first = input("Enter first name: ")
+mid = input("Enter second name: ") + ' '
+last = input("Enter last name: ")
+print("Nice to meet you, " + first + ' ' + mid + last)
+# 6. Bigger, better favorite number. Write a program that
+# asks for a person’s favorite number. Have your program 
+# add 1 to the number, and then suggest the result as a 
+# bigger and better favorite number. (Do be tactful 
+# about it, though.)
