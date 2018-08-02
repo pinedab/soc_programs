39ade7d00b5fbfd40b4ce0cbe4958c63eca45a7b
pinedab <brandyy_9490@hotmail.com>
2018-08-02 01:35:44 -0500

    added hw, for loops, input q
    ./x
    
    /X
    \X
    EXIT
    
    QUIT
    help
    ?
    :wq
    :q
    love.py :Q
    :S
    :X
    :Q
    i
    3*6
    :X!
    :x!
    :q!

--- day3/love.py
+++ day3/love.py
@@ -7,13 +7,18 @@
 # print(a[2]) #p
 # print(a[-1]) #y
 
-# FOR LOOP
-name = "Arcalis Bellanueve"
-result = ""
+##############FOR LOOP
+# name = "Arcalis Bellanueve"
+# result = ""
 
-for i in range(0, len(name)):
-  # print(name[i])
-  if i % 2 == 0: #every second letter
-    print(name[i])
-    result = result + name[i]
-    print('result just changed to: ' + result)
+# for i in range(0, len(name)):
+#   # print(name[i])
+#   if i % 2 == 0: #every second letter
+#     print(name[i])
+#     result = result + name[i]
+#     print('result just changed to: ' + result)
+
+#############INPUT()
+# age = input("What is your age")
+# age = int(age)
+# print(age)
