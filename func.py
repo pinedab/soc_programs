6f0511a7db9f899c5183049a56ab35c237de9882
pinedab <brandyy_9490@hotmail.com>
2018-08-08 15:24:06 -0500

    reading files/recursive func

--- func.py
+++ func.py
@@ -1,11 +1,24 @@
-# SOC Week 2 Day 1
+## SOC Week 2 Day 1
 
-# Functions, parameters
+## Functions, parameters
 def woof(n):
 	# print('woof ' * n)
 	return 'woof ' * n
 
-# Testing
+def ask_recursively(q):
+	print(q)
+	reply = input('> ').lower()
+	if reply == 'yes':
+		return True
+	if reply == 'no':
+		return False
+	else:
+		print("Please answer 'yes' or 'no'.")
+		ask_recursively(q)
+
+ask_recursively('Do you like Mexican food?')
+
+## Testing
 # manual testing
 # for x in range(3):
 # 	woof(x)
@@ -16,5 +29,14 @@ def woof(n):
 # w appropiate imports
 
 
-
 # Reading and Writing Files
+filename = "alice_in_wonderland.txt"
+file = open(filename,"r", encoding ="utf8") # loads it in to our program
+# for line in file:
+# OPENS FILE SEQUENTIALLY, NOTHING LEFT TO READ AFTER print()
+# 	print(line)
+
+raw = file.read()
+print(raw[:65]) # reads char 0 until char 65
+print(f"The length of this book is {len(raw)} characters!")
+# COUNTING == FREQUENCY DISTRIBUTION
---
+++ hw/soc02hw-brandy-pineda.py
@@ -0,0 +1,8 @@
+##### Things to Try:
+
+##### DAY1 #####
+# 1. Calculate a table for each letter in the alphabet 
+# from a-z, and count how many times each letter appears
+# in alice_in_wonderland.txt (fancy word for counting
+# stuff is "frequency distribution" - because you are 
+# counting the frequency of something).
