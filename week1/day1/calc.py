f6e65c885ee65a13e1a0eb7ca15d20f520980fb4
pinedab <brandyy_9490@hotmail.com>
2018-08-08 10:21:39 -0500

    reorganization of w1 files

---
+++ week1/day1/calc.py
@@ -0,0 +1,66 @@
+# calc
+
+#print(1+2);
+#print(3);
+
+####SIMPLE ARITHMETIC
+print('###NUMBERS')
+print(1+2)
+print(2*3)
+print(5-8)
+print(9/2)
+
+print()
+print(5 * (12-8) + -15)
+print(98 + (59872 / (13*8)) * -51)
+
+# power and bitshift
+print()
+print(2**8)
+print(1<<9)
+
+#modulo: gives remainder
+print()
+print(10%2)
+print(10%3)
+print(11%2)
+print(11%3)
+for i in range(0,9):
+	print("bitshift ", i, "times", 1<<i)
+print()
+print('###SRINGS')
+print()
+print("Hello, world!")
+print('')
+print('Good-bye.')
+
+# Doesn't work bc 2 and 5 are strings
+# print('2' * '5')
+# Does work bc multiplying integers
+# print(2 * 5)
+
+print()
+print('###Multiplying strings')
+print('blink ' * 4)
+#blink blink blink blink
+
+print('moo'*3)
+# 'moomoomoo'
+
+###APOSTROPHES
+# YES print('moo\'S')
+# YES print("moo's")
+# NO print('moo/'s')
+# print('yes\\no')   ---> yes\no == print('yes\no')
+
+
+
+
+
+
+
+
+
+
+
+
