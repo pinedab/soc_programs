b84909239895b421d1baef5d518e562cc318c6ad
pinedab <brandyy_9490@hotmail.com>
2018-08-08 11:43:28 -0500

    Python the hard way exercises

---
+++ pthw/ex1.py
@@ -0,0 +1,17 @@
+###### Python the Hard Way #####
+###### #1mwtt sprinkles #####
+
+# Exercise 1
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+# Study Drills
+print("Another line")
+#print("This will not print")
+print("Back to printing")
+
---
+++ pthw/ex2.py
@@ -0,0 +1,17 @@
+###### Python the Hard Way #####
+###### #1mwtt sprinkles #####
+
+# Exercise 2
+print("Comments are started with '#'")
+# print("This won't show") 
+print('Code like this is one way') # commenting...
+print("This will run")
+
+# Study Drills
+print("The symbol '#' is called an:\n octothorpe")
+#print("This will not print")
+print("Back to printing")
+print("...\nCheckcing for mistakes...\n...")
+#Reading backwards
+print("None found :D")
+
