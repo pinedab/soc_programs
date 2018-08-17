84b57c92f4655535352adeb62f60c3d21d7f89ae
pinedab <brandyy_9490@hotmail.com>
2018-08-16 19:49:24 -0500

    working on pthw exercises

--- week2/d1/pthw/ex30.py
+++ week2/d1/pthw/ex30.py
@@ -1,1 +1,24 @@
 # Exercise 30 - Else and If
+people = 30
+cars = 40
+trucks = 10 + 5
+
+
+if cars > people:
+    print("We should take the cars.")
+elif cars < people:
+    print("We should not take the cars")
+else:
+    print("We can't decide!")
+
+if trucks > cars:
+    print("There's too many trucks!")
+elif trucks < cars:
+    print("Maybe we should take the trucks.")
+else:
+    print("We still can't decide!")
+
+if people > trucks:
+    print("Alright, let's just take the trucks.")
+else:
+    print("Fine, let's stay home then.")
---
+++ week2/d1/pthw/ex32.py
@@ -0,0 +1,1 @@
+# Exercise 32 - Loops and lists
