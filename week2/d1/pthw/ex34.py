894257034aeaf93d691fd7dc95c6370d53ca18d2
pinedab <brandyy_9490@hotmail.com>
2018-08-17 22:37:55 -0500

    working on pthw exercises

--- week2/d1/pthw/ex34.py
+++ week2/d1/pthw/ex34.py
@@ -1,1 +1,18 @@
 # Exercise 34 - Accessing Elements of Lists
+# Practice
+"""
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+The animal at 1			<=====>		python
+The third animal 		<=====>		peacock
+The first animal 		<=====>		bear
+The animal at 3			<=====>		kangaroo
+The 5th animal 			<=====>		whale
+The animal at 2 		<=====>		peacock
+The 6th animal 			<=====>		platypus
+The animal at 4			<=====>		whale
+"""
+
+# Study Drills
+# 1. The year 2010 is really year 2010 and not 2009
+# because years are cardinal, not ordinal?
---
+++ week2/d1/pthw/ex35.py
@@ -0,0 +1,83 @@
+# Exercise 35 - Branches and Functions
+from sys import exit
+
+
+def gold_room():
+    print("This room is full of gold. How much do you take?")
+
+    choice = input("> ")
+    if choice.isdigit():
+        how_much = int(choice)
+    else:
+        dead("Man, learn to type a number. You starved with all the gold.")
+
+    if how_much < 50:
+        print("Nice, you're not greedy, you win!")
+        exit(0)
+    else:
+        dead("You greedy bastard!")
+
+
+def bear_room():
+    print("There is a bear here.\nThe bear has a bunch of honey.")
+    print("The fat bear is in front of another door.")
+    print("How are you going to move the bear?")
+    bear_moved = False
+
+    while True:
+        choice = input("> ")
+
+        if choice == "take honey":
+            dead("The bear looks at you, then slaps your face off.")
+        elif choice == "open door" and not bear_moved:
+            print("The bear is threatened and charges at you.")
+            print("You roll out of the way. As you get up,")
+            print("the bear goes back to guarding the door.")
+        elif choice == "taunt bear" and not bear_moved:
+            print("The bear has moved from the door.")
+            print("You can go through the door now.")
+            bear_moved = True
+        elif choice == "taunt bear" and bear_moved:
+            dead("The bear gets pissed off and chews your leg off.")
+        elif choice == "open door" and bear_moved:
+            gold_room()
+        else:
+            print("I got no idea what that means.")
+
+
+def cthulhu_room():
+    print("Here you see the great evil Cthulhu.")
+    print("He, it, whatever stares at you and you go insane.")
+    print("Do you flee for your life or eat your head?")
+
+    choice = input("> ")
+
+    if "flee" in choice:
+        start()
+    elif "head" in choice:
+        dead("Well, that was tasty!")
+    else:
+        cthulhu_room()
+
+
+def dead(why):
+    print(why, "Great job!")
+    exit(0)
+
+
+def start():
+    print("You are in a dark room.")
+    print("There is a door to your right and left.")
+    print("Which one do you take?")
+
+    choice = input("> ")
+
+    if choice == "left":
+        bear_room()
+    elif choice == "right":
+        cthulhu_room()
+    else:
+        dead("You stumble around until you starve.")
+
+
+start()
